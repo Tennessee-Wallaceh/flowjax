7c0530c12919a739dca3d6e16f93dae0d8000788
tennessee_wallaceh <tennessee.wallaceh@gmail.com>
2022-10-06 09:03:31 +0100

    Add batch trainer

--- flowjax/train_utils.py
+++ flowjax/train_utils.py
@@ -97,6 +97,86 @@ def train_flow(
     return dist, losses
 
 
+def batch_train_flow(
+    key: KeyArray,
+    dist: Distribution,
+    x: Array,
+    condition: Optional[Array] = None,
+    max_epochs: int = 50,
+    max_patience: int = 5,
+    learning_rate: float = 5e-4,
+    batch_size: int = 256,
+    val_prop: float = 0.1,
+    clip_norm: float = 0.5,
+    show_progress: bool = True,
+):
+    """Train a distribution (e.g. a flow) by maximum likelihood with Adam optimizer.
+
+    Args:
+        key (KeyArray): Jax PRNGKey.
+        dist (Distribution): Distribution object, trainable parameters are found using equinox.is_inexact_array.
+        x (Array): Samples from target distribution.
+        condition (Optional[Array], optional): Conditioning variables. Defaults to None.
+        max_epochs (int, optional): Maximum number of epochs. Defaults to 50.
+        max_patience (int, optional): Number of consecutive epochs with no validation loss improvement after which training is terminated. Defaults to 5.
+        learning_rate (float, optional): Adam learning rate. Defaults to 5e-4.
+        batch_size (int, optional): Batch size. Defaults to 256.
+        val_prop (float, optional): Proportion of data to use in validation set. Defaults to 0.1.
+        clip_norm (float, optional): Maximum gradient norm before clipping occurs. Defaults to 0.5.
+        show_progress (bool, optional): Whether to show progress bar. Defaults to True.
+    """
+
+    def loss(dist, x, condition=None):
+        return -dist.log_prob(x, condition).mean()
+
+    @eqx.filter_jit
+    def step(dist, optimizer, opt_state, x, condition=None):
+        loss_val, grads = eqx.filter_value_and_grad(loss)(dist, x, condition)
+        updates, opt_state = optimizer.update(grads, opt_state)
+        dist = eqx.apply_updates(dist, updates)
+        return dist, opt_state, loss_val
+
+    key, subkey = random.split(key)
+
+    inputs = (x,) if condition is None else (x, condition)
+    train_args, val_args = train_val_split(subkey, inputs, val_prop=val_prop)
+
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(clip_norm), optax.adam(learning_rate=learning_rate)
+    )
+
+    best_params, static = eqx.partition(dist, eqx.is_inexact_array)
+    opt_state = optimizer.init(best_params)
+
+    losses = {"train": [], "val": []}  # type: Dict[str, List[float]]
+    
+    loop = tqdm(range(max_epochs)) if show_progress is True else range(max_epochs)
+    for epoch in loop:
+        key, subkey = random.split(key)
+        train_args = random_permutation_multiple(subkey, train_args)
+
+        batch = tuple(a[:batch_size] for a in train_args)
+        dist, opt_state, loss_i = step(dist, optimizer, opt_state, *batch)
+        epoch_train_loss = loss_i.item()
+        losses["train"].append(epoch_train_loss)
+
+        batch = tuple(a[:batch_size] for a in val_args)
+        epoch_val_loss = loss(dist, *batch).item()
+        losses["val"].append(epoch_val_loss)
+
+        if epoch_val_loss == min(losses["val"]):
+            best_params = eqx.filter(dist, eqx.is_inexact_array)
+
+        elif count_fruitless(losses["val"]) > max_patience:
+            print("Max patience reached.")
+            break
+
+        if show_progress:
+            loop.set_postfix({k: v[-1] for k, v in losses.items()})
+
+    dist = eqx.combine(best_params, static)
+    return dist, losses
+
 def train_val_split(key: KeyArray, arrays: List[Array], val_prop: float = 0.1):
     """Train validation split along axis 0.
 
